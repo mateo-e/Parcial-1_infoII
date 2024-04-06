66707598bcc792ea306692f066c873078ee0c9f8
Mateo Echavarria <mateo.echavarriap@udea.edu.co>
2024-04-05 21:25:50 -0500

    Se corrigieron algunos errores: -Cuando el tamaño de la primera matriz aumenta, el resto también debe aumentarse. -se comprueba cuandos "1" existen en la norma para un mejor prevision de errores
    Además, se implementó:
    -comprobaciones iniciales para mejoras de eficiencia
    -nuevos protocolos dentro de los ciclos principales

--- CompararMatrices.cpp
+++ CompararMatrices.cpp
@@ -1,5 +1,6 @@
 #include <math.h>
 
+
 using namespace std;
 
 #define MAYOR 1
@@ -19,17 +20,39 @@ bool comparar(short int ** A, short int ** B, short int A_tam, short int B_tam, 
     f_B = f_A - dif_tam;
     c_B = c_A - dif_tam;
 
-    // cout<<endl<<norma<<endl<<"A: "<<A[f_A][c_A]<<endl<<"B: "<<B[f_B][c_B]<<endl<<endl;
 
+    // en caso que la posicion en B no exista:
+
+
+
+    if ( f_B < 0 || c_B < 0) // en caso de que las coordenadas en la nueva matriz se salgan del rango
+    {
+        B = NULL;
+        return false;
+    }
+
+
+    cout<<endl<<norma<<endl<<"A: "<<A[f_A][c_A]<<endl<<"B: "<<B[f_B][c_B]<<endl<<endl;
 
     if ((A[f_A][c_A] > B[f_B][c_B]) && norma == MAYOR)
+    {
+        cout<<endl<<norma<<endl<<"A: "<<A[f_A][c_A]<<endl<<"B: "<<B[f_B][c_B]<<endl<<endl;
         return true;
+    }
 
     if ((A[f_A][c_A] < B[f_B][c_B]) && norma == MENOR)
+    {
+        cout<<endl<<norma<<endl<<"A: "<<A[f_A][c_A]<<endl<<"B: "<<B[f_B][c_B]<<endl<<endl;
         return true;
+    }
 
     if ((A[f_A][c_A] == B[f_B][c_B]) && norma == IGUAL)
+    {
+        cout<<endl<<norma<<endl<<"A: "<<A[f_A][c_A]<<endl<<"B: "<<B[f_B][c_B]<<endl<<endl;
         return true;
+    }
+
+
 
     return false;
 }
--- RotarEstructura.cpp
+++ RotarEstructura.cpp
@@ -1,6 +1,6 @@
 
 
-short int ** rotar_matriz(short int ** matriz, short int tam)
+short int ** rotar_matriz(short int ** matriz, short int tam , bool eliminar)
 {
     short int cont_filas = tam-1 , cont_columnas = 0 , filas = tam-1;
     short int ** matriz_nueva = new short int *[tam];
@@ -20,11 +20,14 @@ short int ** rotar_matriz(short int ** matriz, short int tam)
         cont_filas = tam-1;
     }
 
-    //se borra la matriz original
-    for (int i = 0; i < filas; ++i) {
-        delete[] matriz[i]; // Borra cada fila de la matriz
+    if(eliminar)
+    {
+        //se borra la matriz original
+        for (int i = 0; i < filas; ++i) {
+            delete[] matriz[i]; // Borra cada fila de la matriz
+        }
+        delete[] matriz; // Borra el arreglo de punteros que apuntan a las filas
     }
-    delete[] matriz; // Borra el arreglo de punteros que apuntan a las filas
 
     return matriz_nueva;
 }
